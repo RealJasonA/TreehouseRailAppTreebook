af9c6c1945e77050bf65e04f406b31c0acaa8ce5
Jason <literary@yahoo.com>
2016-03-27 18:19:22 -0600

    Added gravatar but still doesn't work in Rails 4.

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'devise'
 gem 'simple_form'
 
+# To use Attr_access protection
+gem 'protected_attributes'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_name
+
+
+
+
     has_many :statuses
+
+    def full_name
+        first_name + " " + last_name
+    end
+
+    def gravatar_url
+        stripped_email = email.stripped
+        downcased_email = stripped_email.downcase
+        hash = Digest::MD5.hexdigest(downcased_email)
+
+        "http://gravatar.com/avatar/#{hash}"
+    end
+    
 
 end
